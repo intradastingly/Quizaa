96c294b2e9d224d51f0cee24b8f64a40c5ae2ec6
lilgujj <gustavkarlandersson@hotmail.com>
2021-01-20 13:55:39 +0100

    made an array with silly bots answer

--- Scripts/bots.js
+++ Scripts/bots.js
@@ -1,12 +1,29 @@
 
 
-function sillybot(){
+function sillybot(randomNumber){
+    let sillyBotArray = [];
+    let sillyBotUl = document.getElementById("sillyBotUl");
+
     let sillyBotLi = document.createElement("li");
     let sillyBotAnswer = Math.floor(Math.random() * 10)
-    let textNode = document.createTextNode(sillyBotAnswer)
-    sillyBotLi.appendChild(textNode);
-    let sillyBotUl = document.getElementById("sillyBotUl");
-    sillyBotUl.appendChild(sillyBotLi);
+    sillyBotArray.push(sillyBotAnswer);
+    
+    // let textNode = document.createTextNode(sillyBotAnswer)
+    for (let i=0; i < sillyBotArray.length; i++){
+        let botAnswer = document.createTextNode(sillyBotArray[i])
+        sillyBotLi.appendChild(botAnswer);
+        console.log(sillyBotArray)
+        sillyBotUl.appendChild(sillyBotLi);
+    }
+    // let textNode = document.createTextNode(sillyBotAnswer)
 
-    console.log(sillyBotAnswer);
+    console.log(sillyBotAnswer + " = Silly Bots Guess");
+    console.log(randomNumber + " = randomNumber")
 }
+
+
+// create list item
+// add list item to array
+// show list item 
+// present higher or lower or correct
+// move to next bot
