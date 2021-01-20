423a5f3695d50f54177145cedfc433e5acdbce6d
Intradastingly <asaf_lackgren@hotmail.com>
2021-01-20 11:27:15 +0100

    removed console logs

--- Scripts/GameLogic.js
+++ Scripts/GameLogic.js
@@ -1,7 +1,6 @@
 // global variables
 let randomNumber = Math.floor(Math.random() * 100);
 let points = 0;
-console.log(randomNumber)
 
 function eventListeners(){ 
     const submit = document.getElementById('submitGuess');
@@ -17,15 +16,12 @@ function playGame() {
     let inputNumber = Number(takeInput());
     let presentText = document.getElementById("presentText");
     if(inputNumber > randomNumber){
-        console.log("lower")
         presentText.innerHTML = "Lower"
     } if(inputNumber < randomNumber){
-        console.log("higher")
         presentText.innerHTML = "higher"
     } if(inputNumber === randomNumber){
         presentText.innerHTML = "You Win"
         points = points + 1;
         randomNumber = Math.floor(Math.random() * 100);
-        console.log(randomNumber)
     }
 }
