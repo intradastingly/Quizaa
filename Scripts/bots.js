9d75d8a69d9b3e72b517334425616fa5abd837a2
lilgujj <gustavkarlandersson@hotmail.com>
2021-01-20 13:39:27 +0100

    working files

--- Scripts/bots.js
+++ Scripts/bots.js
@@ -1,12 +1,12 @@
 
 
-// function sillybot(){
-//     let sillyBotLi = document.createElement("li");
-//     let sillyBotAnswer = Math.floor(Math.random() * 10)
-//     let textNode = document.createTextNode(sillyBotAnswer)
-//     sillyBotLi.appendChild(textNode);
-//     let sillyBotUl = document.getElementById("sillyBotUl");
-//     sillyBotUl.appendChild(sillyBotLi);
+function sillybot(){
+    let sillyBotLi = document.createElement("li");
+    let sillyBotAnswer = Math.floor(Math.random() * 10)
+    let textNode = document.createTextNode(sillyBotAnswer)
+    sillyBotLi.appendChild(textNode);
+    let sillyBotUl = document.getElementById("sillyBotUl");
+    sillyBotUl.appendChild(sillyBotLi);
 
-//     console.log(sillyBotAnswer);
-// }
+    console.log(sillyBotAnswer);
+}
