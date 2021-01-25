2130746c799e02e541a68f3ccfde37e965539b74
Intradastingly <asaf_lackgren@hotmail.com>
2021-01-25 14:17:36 +0100

    win lose navigation works

--- Scripts/win-lose.js
+++ Scripts/win-lose.js
@@ -3,16 +3,20 @@ function displayWin(){
     let hideGame = document.getElementById('userBox')
     let bot= document.getElementById('sillyBot');
     let win= document.getElementById('you-won');
+    let again = document.getElementById('again');
     hideGame.style.display = "none";
     bot.style.display = "none";
     win.style.display = "flex";
+    again.addEventListener('click', () => {location.reload()});
 }
 
 function displayLose(){
     let hideGame = document.getElementById('userBox')
     let bot= document.getElementById('sillyBot');
     let lose= document.getElementById('you-lose');
+    let again = document.getElementById('again');
     hideGame.style.display = "none";
     bot.style.display = "none";
     lose.style.display = "flex";
+    again.addEventListener('click', () => {location.reload()});
 }
